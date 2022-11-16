f32787e953de5dea19858768b4c75ddb6248995d
jwson <sonjungwoo9@gmail.com>
2022-11-16 14:23:10 +0900

    [Feat] 버튼 동작을 위한 변수 설정
    static final List<Widget> _widgetOptions = <Widget>[]

--- lib/screens/bottom.dart
+++ lib/screens/bottom.dart
@@ -9,6 +9,15 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Ticket"),
+    const Text("Profile"),
+  ];
+  // 바텀 네비게이션 바의 상태(state)를 바꾸기 위해서 변수를 선언해준다.
+  // 총 4개의 버튼을 생성했기 때문에, 변수 타입은 List<Widget>
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,12 +33,18 @@ class _BottomBarState extends State<BottomBar> {
             unselectedItemColor: Color(0xFF526480),
             selectedItemColor: Colors.blueGrey,
             items: [
-        BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
+        BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
             activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled)
             ,label: "1"),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: "2"),
-        BottomNavigationBarItem(icon: Icon(Icons.phone), label: "3"),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: "4"),
+              BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled)
+                  ,label: "2"),
+              BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled)
+                  ,label: "3"),
+              BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled)
+                  ,label: "4"),
       ]
         ),
     );
